3568b841b0e5f759f3d7c8a0ef3eb1075d03a49e
wpcompsci <87338777+wpcompsci@users.noreply.github.com>
2023-07-17 14:12:55 -0400

    2nd commit

---
+++ printing_stuff.py
@@ -0,0 +1,35 @@
+# <- This starts a comment
+# COMMENTS ARE NOT READ BY THE COMPUTER
+
+# PRINT STATEMENT
+# print(whatever we want to print)
+
+print("HELLO") # STR or STRING
+print('WORLD') # STRING
+print(12)      # INT or INTEGER
+print(12.4)    # FLOAT
+print(True)    # BOOL or BOOLEAN
+print(False)   # BOOLEAN
+
+# VARIABLES
+# NAMED PLACES THAT STORE DATA
+
+tim = "This is Tim's money."
+num_bananas = 12
+is_yellow = True
+
+print(tim)
+print(num_bananas)
+num_bananas = num_bananas + 2
+print(num_bananas)
+
+
+# INPUT (GET INFO FROM THE USER)
+# INPUT SHOULD BE SAVED IN A VARIABLE
+
+user_name = input("What is your name? ")
+print(f"Hello {user_name}.")
+
+number = int(input("Enter a number... "))
+new_number = number + 2
+print(f'Your number plus two is {new_number}.')
